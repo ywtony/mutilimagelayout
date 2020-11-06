db206999b37ff66e100098d2b1246d57e50ebfef
wei.yang <wei.yang@wedo99.com>
2020-11-06 19:34:36 +0800

    不规则布局

---
+++ app/src/main/java/com/yw/imagesadapterview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.yw.imagesadapterview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+ /**
+   * @Description:    多图片适配器测试
+   * @Author:         wei.yang
+   * @CreateDate:     2020/10/24 13:07
+   * @UpdateUser:     更新者：wei.yang
+  */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
